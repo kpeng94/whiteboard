ea1dc578d8015bcfbedea5f886c21118cd038d7e
Parker Zhao <pzarker@mit.edu>
2013-12-08 18:19:45 -0500

    Updated User, added draw message

--- src/canvas/User.java
+++ src/canvas/User.java
@@ -6,9 +6,9 @@ import java.awt.Point;
 public class User {
 
 	private Whiteboard whiteboard;
-	private int thickness;
+	private int thickness = 1;
 	private final String username;
-	private Color color;
+	private Color color = Color.black;
 	
 	public User(String username) {
 		this.username = username;
@@ -19,8 +19,13 @@ public class User {
 	}
 	// We could have more ways to set the color
 	// Could play around with brighter() and darker() methods of Color
-	public void setColor(float h, float s, float b) {
-		color = Color.getHSBColor(h, s, b);
+	public void setColor(int r, int g, int b) {
+		color = new Color(r, g, b);
+	}
+	
+	public void setColor(String colorName) {
+		//set color based on color names:
+		//black, magenta, white, etc.
 	}
 	
 	public Color getColor() {
@@ -35,12 +40,24 @@ public class User {
 		return this.thickness;
 	}
 	
-	public void sendLineSegment(Point point1, Point point2, Color c, int thick) {
-		//send the line segment message
+	// returns a String of form "draw [int] [int] [int] [int] from [int] [int] to [int] [int]"
+	public String sendLineSegment(Point point1, Point point2, Color c, int thick) {
+		String red = Integer.toString(c.getRed());
+		String green = Integer.toString(c.getGreen());
+		String blue = Integer.toString(c.getBlue());
+		String t = Integer.toString(thick);
+		String pt1x = Integer.toString(point1.x);
+		String pt1y = Integer.toString(point1.y);
+		String pt2x = Integer.toString(point2.x);
+		String pt2y = Integer.toString(point2.y);
+		
+		return "draw " + red + " " + green + " " + blue + " " + t + " from " + pt1x + " " + pt1y 
+				+ " " + pt2x + " " + pt2y;
+		
 	}
 	
 	public void switchWhiteboard(Whiteboard board) {
-		this.whiteboard = board;
+		//disconnect + join
 	}
 	
 	public void createWhiteboard(Whiteboard board) {
