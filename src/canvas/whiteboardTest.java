36886fbcfda634f124c76b05b75a9e351298f140
Parker Zhao <pzarker@mit.edu>
2013-12-10 23:34:12 -0500

    More tests on whiteboard

--- src/canvas/whiteboardTest.java
+++ src/canvas/whiteboardTest.java
@@ -6,14 +6,19 @@ import java.util.ArrayList;
 
 import org.junit.Test;
 
+/**
+ * Tests that methods of the whiteboard ADT function correctly.
+ * Tests: getName(), setName(), addUsers(), getUsers(), removeUser()
+ * Types of strings to test: empty string, special characters, normal names
+ *
+ */
 public class whiteboardTest {
 	@Test
 	public void testGetSetName() {
-		String name = "";
 		int width = 800; int height = 600;
 		Canvas canvas = new Canvas(width, height);
 		ArrayList<String> users = new ArrayList<String>();
-		Whiteboard whiteboard = new Whiteboard(name, canvas, users);
+		Whiteboard whiteboard = new Whiteboard("", canvas, users);
 		assertEquals("", whiteboard.getName());
 		
 		whiteboard.setName("name");
@@ -25,6 +30,20 @@ public class whiteboardTest {
 	
 	@Test
 	public void testAddGetUsers() {
+		int width = 800; int height = 600;
+		Canvas canvas = new Canvas(width, height);
+		ArrayList<String> users = new ArrayList<String>();
+		Whiteboard whiteboard = new Whiteboard("", canvas, users);
+		whiteboard.addUser("genghis");
+		whiteboard.addUser("kevin");
+		whiteboard.addUser("");
+		whiteboard.addUser(" ");
+		whiteboard.removeUser("kevin");
 		
+		ArrayList<String> result = new ArrayList<String>();
+		result.add("genghis");
+		result.add("");
+		result.add(" ");
+		assertEquals(result, whiteboard.getUsers());
 	}
 }
